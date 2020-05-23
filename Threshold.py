0a4ae94defd8897651ba8c62e636c7534d700299
hex-plex <sendhilsom7@gmail.com>
2020-05-23 13:49:39 +0530

    Threshold

---
+++ Threshold.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy
+
+def Threshold(img):
+    #This piece should threshold the four corners as image_points
+    #as i have not been able to devise any method to do it consitently i am leaving it blank for now
+
+    #default
+    return np.array([[0,0]])
