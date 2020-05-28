7bc4df74d495ada91a7fec5f1c6f33ec79349d4f
hex-plex <56990337+hex-plex@users.noreply.github.com>
2020-05-28 12:14:13 +0530

    Update Threshold.py

--- Threshold.py
+++ Threshold.py
@@ -4,7 +4,9 @@ import numpy
 def threshold(img):
     #This piece should threshold the four corners as image_points
     #as i have not been able to devise any method to do it consitently i am leaving it blank for now
-
+    #green cutouts with ir led in them for thresholding and centers to match
+    #This should be a good method rather than the normal ir
+    #Else the aruco markers are good choice but they are computationally expensive
     #default
     return np.array([[0,0]])
 
