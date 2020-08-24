b2c448f3f25afe1f28a9504e1838dcf90141a0f7
HexPlex <sendhilsom7@gmail.com>
2020-08-24 23:52:04 +0530

    Point Config

--- Raspi-c/PointCalibration.cpp
+++ Raspi-c/PointCalibration.cpp
@@ -18,15 +18,26 @@ int cofigPoints(cv::VideoCapture &cap){
             flag=false;break;
         }
         cv::imshow("Display",image);
-        if(cv::waitKey(1)&255==5){
+        if(cv::waitKey(1)&255==70){
             break;
         }
     }
     if(!flag){return -1;}
+    PointSet rad;
     std::vector<cv::Point3d> imgpoints;
     if(thresh(image,imgpoints,true)){
         std::cout<<"Points were detected \n";
+        std::cout<<"Select a rectangle which defines the inner part of the display the best\n";
+        cv::Rect r = cv::selectROI(image);
+        rad.pntrvec = imgpoints;
+        std::vector<cv::Point2d> selpoints;
+        selpoints.push_back(cv::Point2d(r.x,r.y));selpoints.push_back(cv::Point2d(r.x+r.width,r.y));selpoints.push_back(cv::Point2d(r.x,r.y+r.height));selpoints.push_back(cv::Point2d(r.x+r.width,r.y+r.height));
+        rad.cnrvec = selpoints;
+        cv::FileStorage fs("refpoints.yaml",cv::FileStorage::WRITE);
+        rad>>fs;
+        fs.release();
+        std::cout<<"Completed!!\n";
     }
     else{std::cout<<"retry with all the points in the frame and with good lighting conditions dont over expose near the points!\n";return -1;}
-
+    return 1;
 }
