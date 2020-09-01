bb73ada60eec354bef5ef002ef05b96f18b9c0ef
HexPlex <sendhilsom7@gmail.com>
2020-09-01 10:51:12 +0530

    Errors removed

--- Raspi-c/tools/rfcomm-server.cpp
+++ Raspi-c/tools/rfcomm-server.cpp
@@ -5,19 +5,20 @@
 #include <bluetooth/rfcomm.h>
 
 int main(int argc, char** argv){
-    struct sockaddr_rc loc_addr = [0], rem_addr = {0};
+    struct sockaddr_rc loc_addr = {0}, rem_addr = {0};
     char buf[1024]= {0};
-    int s,client = byte_ord ;
+    int s,client, bytes_read ;
     socklen_t opt = sizeof(rem_addr);
-    s = socker(AF_BLUETOOTH,SOCK_STEAM,BT_PROTO_ADDR );
+    s = socket(AF_BLUETOOTH,SOCK_STREAM,BTPROTO_RFCOMM);
     loc_addr.rc_family = AF_BLUETOOTH;
-    loc_addr.rc_bdaddr = *BDADDR_ANY;
-    loc_addr.rc_channel = (uint8_t) ;
+    bdaddr_t tmp ={};
+    loc_addr.rc_bdaddr = tmp;
+    loc_addr.rc_channel = (uint8_t) 1;
     bind(s,(struct sockaddr *)&loc_addr, sizeof(loc_addr));
     listen(s,1);
-    client = accep(s,(struct sockadd *)&rem_addr,&opt);
-    ba2str(&rem_addr,rc_badder,buf);
-    fprint(stderr,"accepted connection from %s\n",buff);
+    client = accept(s,(struct sockaddr *)&rem_addr,&opt);
+    ba2str(&rem_addr.rc_bdaddr,buf);
+    fprintf(stderr,"accepted connection from %s\n",buf);
     memset(buf,0,sizeof(buf));
 
     bytes_read = read(client,buf,sizeof(buf));
