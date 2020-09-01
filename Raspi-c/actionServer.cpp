05c9fbf14576da33dc1b76762c65e1156e6566a3
HexPlex <sendhilsom7@gmail.com>
2020-09-01 12:07:54 +0530

    ActionServer

--- Raspi-c/actionServer.cpp
+++ Raspi-c/actionServer.cpp
@@ -1,3 +1,29 @@
+#include <iostream>
 #include <wiringPi.h>
-wiringPiISR(4,INT_EDGE_BOTH,void append_request());
-append_request(){};
+
+using namespace std;
+int ctr = 0;
+int flag = 0;
+unsigned int prevTime  = millis();
+vector< void (*)(void) > callbacks;
+void switchDown(void);
+int setupSwitch(vector<void (*)(void)> &cal){
+    wiringPiSetup();
+    callbacks = cal;
+    pinMode(1, INPUT);
+    wiringPiISR(1,INT_EDGE_BOTH,switchDown);
+    while(1){
+        delay(1000);
+    }
+    return 0;
+}
+void switchDown(void){
+    if(flag==1&&!digitalRead(1)&&((millis()-prevTime)>50)){
+        for(void (*)(void) item : callbacks ){
+            item();
+        }
+        flag=0;ctr++;prevTime = millis();
+    } else if(flag==0&&digitalRead(1)&&(millis()-prevTime > 50)){
+        flag=1;prevTime = millis();
+    }
+}
