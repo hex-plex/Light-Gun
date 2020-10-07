4090da0d949137de0a58fa6fe153654cf017db34
HexPlex <sendhilsom7@gmail.com>
2020-10-07 20:45:56 +0530

    Structurizing

--- Raspi-c/src/main.cpp
+++ Raspi-c/src/main.cpp
@@ -1,7 +1,8 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
-#include "actionServer.cpp"
+//#include "actionServer.cpp"
 int main(){
+	std::cout<<"asdfasdfasdff";
     return 0;
 }
