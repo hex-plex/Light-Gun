21f897870f380f93809585c4cdbd402716a6254f
hexplex <sendhilsom7@gmail.com>
2020-12-07 17:59:55 +0530

    ]

---
+++ Raspi-c/src/switch.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<wiringPi.h>
+
+using namespace std;
+unsigned int prevTime = millis();
+void shutdown(void);
+int main(){
+    wiringPiSetup();
+    pinMode(4, INPUT);
+    digitalWrite(4, LOW);
+   // switchinterupt();
+    //wiringPiISR(1,INT_EDGE_RISING,switchup);
+    wiringPiISR(4,INT_EDGE_RISING,shutdown);
+    while(1){
+        delay(500);
+    }
+    return 0;
+}
+void shutdown(void){
+    if((millis()-prevTime)>50 && digitalRead(4)==HIGH){
+        prevTime=millis();
+        cout<<"Shutting Down ... .. .\n";
+    }
+}
