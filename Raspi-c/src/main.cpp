6649e9f2a1c81217aaccc2cf198e6634ea331bc6
hex-plex <sendhilsom7@gmail.com>
2021-01-21 20:12:47 +0530

    CHanges

--- Raspi-c/src/main.cpp
+++ Raspi-c/src/main.cpp
@@ -8,7 +8,7 @@
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
-#include "actionServer.cpp"
+//#include "actionServer.cpp"
 int main(){
 	std::string baddr;
 	std::ifstream myfile("../.configDevice");  //remove ../ please
@@ -18,11 +18,11 @@ int main(){
 		std::cout<<"Bluetooth address config not found read manual to set it up\n";
 		return -1;
 	}
-    actionServer::init(pair_socket);
+    /*actionServer::init(pair_socket);
     actionServer::button_set(compute);
     while(actionServer::ok()){
         actionServer::fetch();
-    }
+    }*/
     return 0;
 }
 
