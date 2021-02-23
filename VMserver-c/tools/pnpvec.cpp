0fa2aed0b27b1c6b08d1e079299f7541fe16e255
hex-plex <sendhilsom7@gmail.com>
2021-02-24 02:20:28 +0530

    Updated functions

--- VMserver-c/tools/pnpvec.cpp
+++ VMserver-c/tools/pnpvec.cpp
@@ -1,59 +1,106 @@
 #include <opencv2/opencv.hpp>
+#include "cameraData.cpp"
+#include "thresh.cpp"
 
 using namespace std;
 using namespace cv;
 
 int main(int argc, char **argv){
-    cv::Mat im = cv::imread("headPose.jpg");
+    //cv::Mat im = cv::imread("headPose.jpg");
+    cv::VideoCapture cap;
+    cap.open(2,cv::CAP_ANY);
+    if(!cap.isOpened()){
+        std::cout<<"Camera couldnt be opened\n";
+        return -1;
+    }
+    
+    std::cout<<"Please press F to select the frame when camera is parrallel to the display\n";
+    cv::Mat im;
+
+    bool flag=true;
+    cv::namedWindow("Display",cv::WINDOW_AUTOSIZE);
+    while(true){
+        cap>>im;
+        if(!im.data){
+            std::cout<<"No image is received from the source\n";
+            flag=false;break;
+        }
+        cv::imshow("Display",im);
+        if(cv::waitKey(1)==102){
+            break;
+        }
+    }
+
+    int res[2] = {im.cols, im.rows};
+
+    PointSet r;
+    CameraParams c;
+    cv::FileStorage fs("refpoints.yaml", cv::FileStorage::READ);
+    fs>>r;
+    fs.release();
+
+    cv::FileStorage ds("configuredInfo.yaml", cv::FileStorage::READ);
+    ds>>c;
+    ds.release();
 
     std::vector<cv::Point2d> image_points;
-    image_points.push_back(cv::Point2d(359,391));
-    image_points.push_back(cv::Point2d(399, 561));
-    image_points.push_back(cv::Point2d(337, 297));
-    image_points.push_back(cv::Point2d(513, 301));
-    image_points.push_back(cv::Point2d(345, 465));
-    image_points.push_back(cv::Point2d(453, 469));
 
-    std::vector<cv::Point3d> model_points;
-    model_points.push_back(cv::Point3d(0.0f, 0.0f, 0.0f));
-    model_points.push_back(cv::Point3d(0.0f, -330.0f, -65.0f));
-    model_points.push_back(cv::Point3d(-255.0f, 170.0f, -135.0f));
-    model_points.push_back(cv::Point3d(255.0f, 170.0f, -135.0f));
-    model_points.push_back(cv::Point3d(-150.0f, -150.0f, -125.0f));
-    model_points.push_back(cv::Point3d(150.0f, -150.0f, -125.0f));
+    thresh(im, image_points, false, res );
 
-    double focal_length = im.cols;
+    std::vector<cv::Point3d> model_points = r.pntrvec;
+
     Point2d center = cv::Point2d(im.cols/2, im.rows/2);
-    cv::Mat camera_matrix = (cv::Mat_<double>(3,3) << focal_length, 0, center.x, 0, focal_length, center.y, 0 , 0, 1 );
-    cv::Mat dist_coeffs = cv::Mat::zeros(4,1, cv::DataType<double>::type);
+    cv::Mat camera_matrix =  c.cameraMatrix;
+    cv::Mat dist_coeffs = c.distCoeffs;
 
     cout<<"Camera Matrix"<<endl<<camera_matrix<<endl;
     
-    cv::Mat rotation_vector;
-    cv::Mat translation_vector;
+    while(true){
+        cap>>im;
+        if(!thresh(im, image_points, false, res ))continue;
 
-    cv::solvePnP(model_points, image_points, camera_matrix, dist_coeffs, rotation_vector, translation_vector);
+        cv::Mat rotation_vector;
+        cv::Mat translation_vector;
 
-    vector<Point3d> nose_end_point3D;
-    vector<Point2d> nose_end_point2D;
-    nose_end_point3D.push_back(Point3d(0,0,1000.0));
+        cv::solvePnP(model_points, image_points, camera_matrix, dist_coeffs, rotation_vector, translation_vector);
 
-    projectPoints(nose_end_point3D, rotation_vector, translation_vector, camera_matrix, dist_coeffs, nose_end_point2D);
+        vector<Point3d> cen_point3D;
+        vector<Point2d> cen_point2D1;
+        vector<Point2d> cen_point2D2;
+        Point3d temp2(0,0,2000.0);
+        Point3d temp1(0,0,10.0);
+        for(int i=0;i<4;i++){
+            temp1.x+=model_points[i].x;
+            temp1.y+=model_points[i].y;
+            temp2.x+=model_points[i].x;
+            temp2.y+=model_points[i].y;
+        }
+        temp1.x/=4;
+        temp1.y/=4;
+        temp2.x/=4;
+        temp2.y/=4;
+        cen_point3D.push_back(temp1);
+    
+        projectPoints(cen_point3D, rotation_vector, translation_vector, camera_matrix, dist_coeffs, cen_point2D1);
+        cen_point3D.clear();
+        cen_point3D.push_back(temp2);
+        projectPoints(cen_point3D, rotation_vector, translation_vector, camera_matrix, dist_coeffs, cen_point2D2);
 
-    for(int i=0; i<image_points.size();i++){
-        circle(im, image_points.at(i), 3, Scalar(0,0,255), -1);
+        for(int i=0; i<image_points.size();i++){
+            circle(im, image_points.at(i), 3, Scalar(0,0,255), -1);
+        }
+
+        cv::line(im, cen_point2D1[0], cen_point2D2[0], cv::Scalar(255,0,0), 2);
+
+        cout<<"Rotation Vector"<<endl<<rotation_vector<<endl;
+        cout<<"Translation Vector"<<endl<<translation_vector<<endl;
+
+        cout<<cen_point2D1<<endl;
+
+        cv::imshow("Output", im);
+        if(cv::waitKey(1)==113)break;
+    
     }
-
-    cv::line(im, image_points[0], nose_end_point2D[0], cv::Scalar(255,0,0), 2);
-
-    cout<<"Rotation Vector"<<endl<<rotation_vector<<endl;
-    cout<<"Translation Vector"<<endl<<translation_vector<<endl;
-
-    cout<<nose_end_point2D<<endl;
-
-    cv::imshow("Output", im);
-    cv::waitKey(0);
-
 
     return 0;
 }
